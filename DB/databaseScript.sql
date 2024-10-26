7d5499df74d9207397af68d15762bcc8c9dd6a33
Mouawaz <99729264+Mouawaz@users.noreply.github.com>
2024-10-26 15:47:33 +0200

    HEAD

---
+++ DB/databaseScript.sql
@@ -0,0 +1,22 @@
+drop schema ReFrigderate;
+create schema ReFrigderate;
+drop table users;
+create table users(
+    userid       varchar(255)    not null
+        primary key,
+    email        varchar(255) not null
+        unique,
+    password     varchar(255) not null,
+    firstname    varchar(255),
+    lastname     varchar(255),
+    dateofbirth  date,
+    sex          char,
+    phonenumber  varchar(20)
+);
+
+ INSERT INTO users (userid, email, password, firstname, lastname, dateofbirth, sex, phonenumber)
+VALUES
+    ('realSigma', 'user2@example.com', 'password2', 'Alice', 'Smith', '1985-05-15', 'F', '9876543210'),
+    ('BetaBehavior', 'user3@example.com', 'password3', 'Bob', 'Johnson', '1978-09-30', 'M', '5551234567'),
+    ('kitchenWench', 'user4@example.com', 'password4', 'Emily', 'Brown', '2000-03-20', 'F', '7779876543'),
+    ('HeeHeee', 'user5@example.com', 'password5', 'Michael', 'Davis', '1995-11-10', 'M', '1112223333');
