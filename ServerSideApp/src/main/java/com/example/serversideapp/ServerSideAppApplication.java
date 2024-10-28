ad0106d581b597e36c6a845cb248971cd191d0c0
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2024-10-28 12:43:46 +0100

    ziggy says not first push

---
+++ ServerSideApp/src/main/java/com/example/serversideapp/ServerSideAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.serversideapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSideAppApplication.class, args);
+    }
+
+}
---
+++ ServerSideApp/src/test/java/com/example/serversideapp/ServerSideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serversideapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
