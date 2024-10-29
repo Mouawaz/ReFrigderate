1cde8e6cf496dad1d047a9c69f95d6fadd74ac86
Mouawaz <99729264+Mouawaz@users.noreply.github.com>
2024-10-29 14:51:01 +0100

    PoliticallyCorrect IDs
    Took 4 minutes

--- DB/databaseScript.sql
+++ DB/databaseScript.sql
@@ -17,7 +17,7 @@ create table users(
 
  INSERT INTO users (userid, email, password, firstname, lastname, dateofbirth, sex, phonenumber)
 VALUES
-    ('realSigma', 'user2@example.com', 'password2', 'Alice', 'Smith', '1985-05-15', 'F', '9876543210'),
-    ('BetaBehavior', 'user3@example.com', 'password3', 'Bob', 'Johnson', '1978-09-30', 'M', '5551234567'),
-    ('kitchenWench', 'user4@example.com', 'password4', 'Emily', 'Brown', '2000-03-20', 'F', '7779876543'),
-    ('HeeHeee', 'user5@example.com', 'password5', 'Michael', 'Davis', '1995-11-10', 'M', '1112223333');
+    ('1', 'user2@example.com', 'password2', 'Alice', 'Smith', '1985-05-15', 'F', '9876543210'),
+    ('2', 'user3@example.com', 'password3', 'Bob', 'Johnson', '1978-09-30', 'M', '5551234567'),
+    ('3', 'user4@example.com', 'password4', 'Emily', 'Brown', '2000-03-20', 'F', '7779876543'),
+    ('4', 'user5@example.com', 'password5', 'Michael', 'Davis', '1995-11-10', 'M', '1112223333');
