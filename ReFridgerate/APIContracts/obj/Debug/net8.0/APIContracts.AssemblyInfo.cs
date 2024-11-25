fdec1cc9cb8752fc1c680a9d1aaa0e9d5b58de2e
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2024-11-25 09:25:39 +0100

    hello

--- ReFridgerate/APIContracts/obj/Debug/net8.0/APIContracts.AssemblyInfo.cs
+++ ReFridgerate/APIContracts/obj/Debug/net8.0/APIContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41557be07b8059b17a9c7e0e708d38e484814894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5e3c7b6a043e22590a143c484d796287da758c")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
